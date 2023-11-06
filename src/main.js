e0d8bce6709f4fd2c0f8c80218ec2444ed02faf2
Rogerio Oliveira <rogerioantoniooliveira@gmail.com>
2023-11-06 10:41:54 -0300

    Site finalizado, falta fazer a parte responsiva

--- src/main.js
+++ src/main.js
@@ -2,6 +2,19 @@ document.addEventListener('DOMContentLoaded', function() {
     const buttons = document.querySelectorAll('[data-tab-button]');
     const questions = document.querySelectorAll('[data-faq-question]')
 
+    const sectionHero = document.querySelector('.hero');
+    const alturaHero = sectionHero.clientHeight;
+
+    window.addEventListener('scroll', function() {
+        const posicaoAtual = window.scrollY;
+
+        if (posicaoAtual < alturaHero) {
+            ocultaBotoesHeader();
+        } else {
+            mostraBotoesHeader();
+        }
+    })
+
     for (let i = 0; i < buttons.length; i++) {
         buttons[i].addEventListener('click', function(botao) {
             const optionSelected = botao.target.dataset.tabButton;
@@ -21,6 +34,16 @@ document.addEventListener('DOMContentLoaded', function() {
         questions[i].addEventListener('click', abreFechaResposta);
     }
 })
+
+function ocultaBotoesHeader() {
+    const botoesHeader = document.querySelector('.header__menu__link')
+    botoesHeader.classList.add('header__menu__link--hidden-button')
+} 
+
+function mostraBotoesHeader() {
+    const botoesHeader = document.querySelector('.header__menu__link')
+    botoesHeader.classList.remove('header__menu__link--hidden-button')
+} 
 
 function ocultaTodasAbas() {
     const tabList = document.querySelectorAll('[data-tab-id]')
